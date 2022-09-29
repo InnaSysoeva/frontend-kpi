d2f91b62f9d8d38656053ed2a3017ae58aa7e877
Your Name <you@example.com>
2022-09-29 11:00:39 +0300

    lab1

---
+++ index.js
@@ -0,0 +1,16 @@
+var info = document.getElementById("info-text");
+var skills = document.getElementById("skills-text");
+var education = document.getElementById("education-text");
+function clickInf()
+{
+    info.style.display = "block";
+
+}
+function clickSkills()
+{
+    skills.style.display = "block";
+}
+function clickEdu()
+{
+    education.style.display = "block";
+}
