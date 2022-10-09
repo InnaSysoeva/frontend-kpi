967f26440976f8c74659d9b0a5212d2395cf16de
Your Name <you@example.com>
2022-10-09 20:27:10 +0300

    lab1

--- lab1/index.js
+++ lab1/index.js
@@ -1,16 +1,43 @@
-var info = document.getElementById("info-text");
-var skills = document.getElementById("skills-text");
-var education = document.getElementById("education-text");
-function clickInf()
+var el = document.getElementById("info");
+el.addEventListener("click", showinfoblock, false);
+function showinfoblock() 
 {
-    info.style.display = "block";
+    var info = document.getElementById("info-text");
+    if (info.style.display == "block") 
+    {
+        info.style.display = "none";
+    } 
+    else 
+    {
+        info.style.display = "block";
+    }
+}
+var el2 = document.getElementById("skills");
+el2.addEventListener("click", showskillsblock, false);
+function showskillsblock() 
+{
+    var skills = document.getElementById("skills-text");
+    if (skills.style.display == "block") 
+    {
+        skills.style.display = "none";
+    } 
+    else 
+    {
+        skills.style.display = "block";
+    }
+}
+var el3 = document.getElementById("education");
+el3.addEventListener("click", showedublock, false);
+function showedublock() 
+{
+    var edu = document.getElementById("education-text");
+    if (edu.style.display == "block") 
+    {
+      edu.style.display = "none";
+    } 
+    else 
+    {
+      edu.style.display = "block";
+    }
+}
 
-}
-function clickSkills()
-{
-    skills.style.display = "block";
-}
-function clickEdu()
-{
-    education.style.display = "block";
-}
